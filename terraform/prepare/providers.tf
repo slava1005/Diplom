88180abdf4cacf1554043719acdacb44d5f70166
VJACHESLAV <114395964+slava1005@users.noreply.github.com>
2025-01-03 20:07:57 +0300

    Update providers.tf

--- terraform/prepare/providers.tf
+++ terraform/prepare/providers.tf
@@ -3,12 +3,13 @@ terraform {
     yandex = {
       source = "yandex-cloud/yandex"
     }
-  }
+  }          terraform validate
+
 #  required_version = ">=0.13"
 }
 
 provider "yandex" {
-#  service_account_key_file     = file("~/.ssh/sa-iam.json")
+  service_account_key_file     = filebase64("~/.ssh/sa-iam.json")
   token     = var.yandex_cloud_token  # Для токена
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
