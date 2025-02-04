a753d9f28aea351bb329f9f4ed0c343cfce11fb4
VJACHESLAV <114395964+slava1005@users.noreply.github.com>
2025-02-04 19:13:58 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "yandex" {
-service_account_key_file = "~/.ssh/sa-iam.json"
+service_account_key_file = file("~/.ssh/sa-iam.json")
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.default_zone
